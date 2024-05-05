bc6fd97306be054f4ca2acb933fa0148ae5e3286
Aswani-Achu <aswaniachu082003@gmail.com>
2024-05-05 20:15:24 +0530

    update search

--- src/components/Sidebar/Search.jsx
+++ src/components/Sidebar/Search.jsx
@@ -1,37 +1,89 @@
-import { Box, Link, Tooltip } from "@chakra-ui/react";
-import { IoSearchSharp } from "react-icons/io5";
-import { Link as RouterLink } from "react-router-dom";
+import { SearchLogo } from "../../../public/assets/constants";
+import useSearchUser from "../../hooks/useSearchUser";
+import { useRef } from "react";
+import SuggestedUser from "../../components/SuggestedUsers/SuggestedUser";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Tooltip,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 const Search = () => {
-	return (
-        <Tooltip
-                    hasArrow
-                    label={"Search"}
-                    placement="right"
-                    ml={1}
-                    openDelay={500}
-                    display={{ base: "block", md: "none" }}
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const searchRef = useRef(null);
+  const { user, isLoading, getUserProfile, setUser } = useSearchUser();
+
+  const handleSearchUser = (e) => {
+    e.preventDefault();
+    getUserProfile(searchRef.current.value);
+  };
+  return (
+    <>
+      <Tooltip
+        hasArrow
+        label={"Search"}
+        placement="right"
+        ml={1}
+        openDelay={500}
+        display={{ base: "block", md: "none" }}
+      >
+        <Flex
+          alignItems={"center"}
+          gap={4}
+          _hover={{ bg: "whiteAlpha.400" }}
+          borderRadius={6}
+          p={2}
+          w={{ base: 10, md: "full" }}
+          justifyContent={{ base: "center", md: "flex-start" }}
+          onClick={onOpen}
+        >
+          <SearchLogo />
+          <Box display={{ base: "none", md: "block" }} color={"#127B7E"}>
+            Search
+          </Box>
+        </Flex>
+      </Tooltip>
+      <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInLeft">
+        <ModalOverlay />
+        <ModalContent bg={"white"} border={"1px solid  #127b7e"} maxW={"400px"}>
+          <ModalHeader>Search user</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <form onSubmit={handleSearchUser}>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input placeholder="Find a user" ref={searchRef} />
+              </FormControl>
+
+              <Flex w={"full"} justifyContent={"flex-end"}>
+                <Button
+                  type="submit"
+                  ml={"auto"}
+                  size={"sm"}
+                  my={4}
+                  isLoading={isLoading}
                 >
-                <Link
-                    display={"flex"}
-                    to={"#"}
-                    as={RouterLink}
-                    alignitems={"center"}
-                    gap={4}
-                    _hover={{ bg: "whiteAlpha.400" }}
-                    borderRadius={6}
-                    p={2}
-                    mt={"auto"}
-                    w={{ base: 10, md: "full" }}
-                    justifyContent={{ base: "center", md: "flex-start" }}
-                    >
-                    <IoSearchSharp  size={25} color={"#127B7E"}/>
-                    <Box display={{ base: "none", md: "block" }} color={"#127B7E"}>
-                        Search
-                    </Box>
-                </Link>
-            </Tooltip>
-        );
-    };
-        
+                  Search
+                </Button>
+              </Flex>
+            </form>
+            {user && <SuggestedUser user={user} setUser={setUser} />}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
 export default Search;
